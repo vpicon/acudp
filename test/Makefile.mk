6c33f1aa9dfd4c6cd76aafa96c098de87c37afa4
Victor Picon <vpicon98@gmail.com>
2022-04-21 11:37:12 +0200

    Add null test file

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -1,2 +1,14 @@
-all:
-	@echo "Building tests..."
+LDFLAGS = -L'../lib' -lacudp -lcmocka
+
+SOURCES := $(wildcard unit/*.c)
+EXECS := $(patsubst %.c, %.exe, $(SOURCES))
+
+all: $(EXECS) 
+
+$(EXECS): %.exe: %.c
+	$(CC) $(CFLAGS) -I'../include'  $< -o $@ $(LDFLAGS)
+
+
+.PHONY:	clean
+clean:
+	-rm $(EXECS) example_plus
