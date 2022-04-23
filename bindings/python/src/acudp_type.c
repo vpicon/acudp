4451debc28602100f476aa19afdc3028e9c3cb01
Victor Picon <vpicon98@gmail.com>
2022-04-23 21:23:00 +0200

    Add dealloc method to ACUDP type

--- bindings/python/src/acudp_type.c
+++ bindings/python/src/acudp_type.c
@@ -1,5 +1,11 @@
 #include "acudp_type.h"
 
+static void
+ACUDP_dealloc(ACUDPObject *self)
+{
+    Py_TYPE(self)->tp_free((PyObject *) self);
+}
+
 PyTypeObject ACUDPType = {
     PyVarObject_HEAD_INIT(NULL, 0)
     .tp_name = "acudp.ACUDP",
@@ -8,4 +14,23 @@ PyTypeObject ACUDPType = {
     .tp_itemsize = 0,
     .tp_flags = Py_TPFLAGS_DEFAULT,
     .tp_new = PyType_GenericNew,
+    /* .tp_new = HandshakeResponse_new, */
+    /* .tp_init = (initproc) HandshakeResponse_init, */
+    .tp_dealloc = (destructor) ACUDP_dealloc,
+    /* .tp_members = HandshakeResponse_members */
 };
+
+/*
+PyTypeObject HandshakeResponseType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "acudp.HandshakeResponse",
+    .tp_doc = PyDoc_STR("Reponse object to Handshake operation"),
+    .tp_basicsize = sizeof(HandshakeResponseObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_new = HandshakeResponse_new,
+    .tp_init = (initproc) HandshakeResponse_init,
+    .tp_dealloc = (destructor) HandshakeResponse_dealloc,
+    .tp_members = HandshakeResponse_members
+};
+*/
