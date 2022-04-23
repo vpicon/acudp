d86631f3e5a70a60e01fd16799e5009b48d00975
Victor Picon <vpicon98@gmail.com>
2022-04-23 15:20:40 +0200

    Add stub setup program for acudp py module

---
+++ bindings/python/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup, Extension
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+module = Extension('acudp',
+                    #  define_macros = [('MAJOR_VERSION', '1'),
+                                     #  ('MINOR_VERSION', '0')],
+                    #  include_dirs = ['/usr/local/include'],
+                    #  libraries = ['tcl83'],
+                    #  library_dirs = ['/usr/local/lib'],
+                    sources = ['src/acudpmodule.c'])
+
+setup (name = 'acudp',
+       version = '0.1',
+       description = 'AC UDP client module',
+       long_description=long_description,
+       author = 'Victor Picon',
+       url = '',
+       ext_modules = [module])
