b1f26e27cb64309c8a242e05ccc7d7b007e78af3
Victor Picon <vpicon98@gmail.com>
2022-04-24 11:48:40 +0200

    Add members to CarInfo type

--- python-extension/src/car_info_type.c
+++ python-extension/src/car_info_type.c
@@ -20,18 +20,18 @@ CarInfo_new(PyTypeObject *type,
 
     // Initialize bools to false, empty tuples and numerics to 0
     if (self != NULL) {
-        self->speed_kmh = 0;
-        self->car_position_normalized = 0;
-        self->car_coordinates =  Py_BuildValue("(fff)", 0, 0, 0);  // tuple
+        self->speed_kmh = 0.0;
+        self->car_position_normalized = 0.0;
+        self->car_coordinates =  Py_BuildValue("(fff)", 0.0, 0.0, 0.0);  // tuple
         self->is_in_pit = PyBool_FromLong(0);  // false
 
-        self->gas = 0;
-        self->brake = 0;
-        self->clutch = 0;
-        self->engine_rpm = 0;
-        self->steer = 0;
+        self->gas = 0.0;
+        self->brake = 0.0;
+        self->clutch = 0.0;
+        self->engine_rpm = 0.0;
+        self->steer = 0.0;
         self->gear = 0;
-        self->cg_height = 0;
+        self->cg_height = 0.0;
 
         self->lap_time_millis = 0;
         self->last_lap_millis = 0;
@@ -46,16 +46,37 @@ CarInfo_new(PyTypeObject *type,
 static PyMemberDef CarInfo_members[] = {
     {"speed_kmh", T_FLOAT, offsetof(CarInfoObject, speed_kmh),
         READONLY, "car speed in km/h"},
-    /* {"driver_name", T_OBJECT_EX, offsetof(HandshakeResponseObject, driver_name), */
-        /* READONLY, "driver name"}, */
-    /* {"identifier", T_INT, offsetof(HandshakeResponseObject, identifier), */
-        /* READONLY, "identifier"}, */
-    /* {"version", T_INT, offsetof(HandshakeResponseObject, version), */
-        /* READONLY, "version"}, */
-    /* {"track_name", T_OBJECT_EX, offsetof(HandshakeResponseObject, track_name), */
-        /* READONLY, "track name"}, */
-    /* {"track_config", T_OBJECT_EX, offsetof(HandshakeResponseObject, track_config), */
-        /* READONLY, "track config"}, */
+    {"car_position_normalized", T_FLOAT, offsetof(CarInfoObject, car_position_normalized),
+        READONLY, "car position in circuit normalized (percentage of circuit done)"},
+    {"car_coordinates", T_OBJECT_EX, offsetof(CarInfoObject, car_coordinates),
+        READONLY, "absolute reference car coordinates (x, y, z)"},
+    {"is_in_pit", T_BOOL, offsetof(CarInfoObject, is_in_pit),
+        READONLY, "is car in pit stop"},
+
+    {"gas", T_FLOAT, offsetof(CarInfoObject, gas),
+        READONLY, "gas"},
+    {"brake", T_FLOAT, offsetof(CarInfoObject, brake),
+        READONLY, "brake"},
+    {"clutch", T_FLOAT, offsetof(CarInfoObject, clutch),
+        READONLY, "clutch"},
+    {"engine_rpm", T_FLOAT, offsetof(CarInfoObject, engine_rpm),
+        READONLY, "engine rpm"},
+    {"steer", T_FLOAT, offsetof(CarInfoObject, steer),
+        READONLY, "steer"},
+    {"gear", T_INT, offsetof(CarInfoObject, gear),
+        READONLY, "gear"},
+    {"cg_height", T_FLOAT, offsetof(CarInfoObject, cg_height),
+        READONLY, "cg height"},
+
+    {"lap_time_millis", T_INT, offsetof(CarInfoObject, lap_time_millis),
+        READONLY, "current lap time in milliseconds"},
+    {"last_lap_millis", T_INT, offsetof(CarInfoObject, last_lap_millis),
+        READONLY, "last lap time in milliseconds"},
+    {"best_lap_millis", T_INT, offsetof(CarInfoObject, best_lap_millis),
+        READONLY, "best lap time in milliseconds"},
+    {"lap_count", T_INT, offsetof(CarInfoObject, lap_count),
+        READONLY, "lap_count"},
+
     {NULL}  /* Sentinel */
 };
 
